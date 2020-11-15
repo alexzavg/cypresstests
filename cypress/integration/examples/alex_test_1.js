ac2e1b69033e81ca2e73b19ddd2de5915bcf83ca
oleksandrzavgorodniy <wa12aszx34>
2020-11-15 04:05:50 +0200

    test

--- cypress/integration/examples/alex_test_1.js
+++ cypress/integration/examples/alex_test_1.js
@@ -1,9 +1,5 @@
-import * as data from '../../fixtures/urls.json';
-
 describe('Alex spec example 1', () => {
-    const baseUrl = data.baseUrl;
-
     it('Alex test example 1', () => {
-        cy.visit(baseUrl);
+        cy.visit();
     });
 });
