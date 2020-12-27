f9eeeaf884771341256abc7b1e9542fe3946d578
alexzavg <me41zavgorodnii@gmail.com>
2020-12-27 18:08:51 +0200

    test

--- cypress/integration/2.2.spec.js
+++ cypress/integration/2.2.spec.js
@@ -2,7 +2,20 @@ describe('2.2', function() {
  
     it('should check hexagons', function() {
     
-       cy.visit('https://public.obsidiandemo.com/obsidian/dashboard/?view=strategic');
+        cy.visit('https://test:ObsidianTest9!@public.obsidiandemo.com/obsidian/dashboard/?view=strategic');
+
+       /*
+
+       ! Sorry but I can't automate it because I'm a loser that has no skill.
+
+        * Create a javascript function returning position and color of every hexagon.
+        elem.getBoundingClientRect()
+
+        * Automate the following test case: if one or more hexagon are coloured in red the the test must be KO
+       */
+        cy.get('g.location #service-1').then((result)=>{
+            cy.log(result);
+        });
  
     });
  
