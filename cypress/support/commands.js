9bee7ee33e2b2d1484f22a010f1767475e35d56f
alexzavg <me41zavgorodnii@gmail.com>
2020-12-27 06:48:25 +0200

    added second test
    added test 2

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import {els} from '../support/app-data';
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -12,6 +14,28 @@
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
 //
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('/');
+    
+    cy.get(els.username_input)
+        .click()
+        .clear()
+        .type(username);
+    cy.get(els.username_input)
+        .should('have.value', username);
+    
+    cy.get(els.password_input)
+        .click()
+        .clear()
+        .type(password);
+    cy.get(els.password_input)
+        .should('have.value', password);
+       
+    cy.get(els.login_button)
+       .click();
+});
+
 //
 // -- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
