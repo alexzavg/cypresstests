bdb0cf9f338961a8c6e4303d70daea3ac0b5c13c
alexzavg <me41zavgorodnii@gmail.com>
2020-12-27 07:12:30 +0200

    Create 2.2.spec.js

---
+++ cypress/integration/2.2.spec.js
@@ -0,0 +1,9 @@
+describe('2.2', function() {
+ 
+    it('should check hexagons', function() {
+    
+       cy.visit('https://public.obsidiandemo.com/obsidian/dashboard/?view=strategic');
+ 
+    });
+ 
+ });
